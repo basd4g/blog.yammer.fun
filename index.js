a3fe4fe3e174794877a680294f83a10d09d004fd
Keisuke Nakayama <baskk4.dt@gmail.com>
2020-05-31 15:17:05 +0900

    Add to load enviromental port number

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const app = express();
 
 app.get('/article/*', (req,res) => {
   const tail = req.path.substring('/article/'.length);
-  res.redirect(`https://basd4g.hatenablog.com/entry/${tail}`);
+  res.redirect(301,`https://basd4g.hatenablog.com/entry/${tail}`);
   res.end();
 });
 
 app.get('/*', (req,res) => {
-  res.redirect(`https://basd4g.hatenablog.com`);
+  res.redirect(301,`https://basd4g.hatenablog.com`);
   res.end();
 })
 
-app.listen(3000, () => console.log('listening on port 3000'));
+const port = process.env.PORT || 3000;
+app.listen(3000, () => console.log(`listening on port ${port}`));
